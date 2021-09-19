ac89386e5d3509900fac43b0b156f84d29fcfc7c
AleksSundr <sunsunicha@gmail.com>
2021-09-19 03:57:50 +0200

    003

---
+++ Dayli UI 003/app.js
@@ -0,0 +1,11 @@
+$(function () {
+    /* Nav Toggle */
+    let nav = $("#nav");
+    let navToggle = $("#navToggle");
+
+    navToggle.on("click", function (event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+    });
+});
